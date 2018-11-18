7bc650021e182493eac63c2fe085792c96862048
Tilaye Alemu <tilaye@gmail.com>
2018-11-18 18:15:54 +0000

    configuring travis

--- docker/push.sh
+++ docker/push.sh
@@ -1,4 +1,5 @@
-mkdir docker
-wget https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/tools/dockerfiles/bashrc
+# wget https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/tools/dockerfiles/bashrc
+
+docker login -u=tilayealemu -p=$DOCKERHUB_PASS
 docker build -t tilayealemu/melanet:latest .
 docker push tilayealemu/melanet:latest
