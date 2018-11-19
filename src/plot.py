16495ddd7e48db22c29cd1f9f51730fbe291ffce
Tilaye Alemu <tilaye.alemu@teamaol.com>
2018-11-19 23:17:10 +0000

    plot hist

---
+++ src/plot.py
@@ -0,0 +1,14 @@
+"""
+Helper functions for plotting
+"""
+
+import pickle
+import matplotlib.pyplot as plt
+
+def plot_hist(p):
+    hist = pickle.load(open( "models/" + p + ".pickle", "rb"))
+    plt.plot(hist['loss'], label="train")
+    plt.plot(hist['val_loss'], label="valid")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.show
