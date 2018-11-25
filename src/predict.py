20e36f15c3af9e692148a51ad074448c763cc9ce
Tilaye Alemu <tilaye@gmail.com>
2018-11-25 16:29:49 +0000

    raw predictions for sample data

--- src/predict.py
+++ src/predict.py
@@ -15,22 +15,7 @@ def predict(index, partition, model, verbose=True):
         partition (str): One of 'train' or 'validation'
         model (Model): The acoustic model
     """
-    # load the train and test data
-
-    # obtain the true transcription and the audio features 
-    if partition == 'validation':
-        transcr = data_gen.valid_texts[index]
-        audio_path = data_gen.valid_audio_paths[index]
-        data_point = data_gen.normalize(data_gen.featurize(audio_path))
-    elif partition == 'train':
-        transcr = data_gen.train_texts[index]
-        audio_path = data_gen.train_audio_paths[index]
-        data_point = data_gen.normalize(data_gen.featurize(audio_path))
-    else:
-        raise Exception('Invalid partition!  Must be "train" or "validation"')
-        
-    # obtain and decode the acoustic model's predictions
-    prediction = model.predict(np.expand_dims(data_point, axis=0))
+    audio_path,data_point,transcr,prediction = predict_raw(index, partition, model)
     output_length = [model.output_length(data_point.shape[0])]
     pred_ints = (K.eval(K.ctc_decode(
                 prediction, output_length, greedy=False)[0][0])+1).flatten().tolist()
@@ -42,3 +27,25 @@ def predict(index, partition, model, verbose=True):
         print('Predicted: ' + predicted)
         print("wer: %d" % wer_val)
     return wer_val
+
+def predict_raw(index, partition, model):
+    """ Get a model's decoded predictions
+    Params:
+        index (int): The example you would like to visualize
+        partition (str): One of 'train' or 'validation'
+        model (Model): The acoustic model
+    """
+
+    if partition == 'validation':
+        transcr = data_gen.valid_texts[index]
+        audio_path = data_gen.valid_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    elif partition == 'train':
+        transcr = data_gen.train_texts[index]
+        audio_path = data_gen.train_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    else:
+        raise Exception('Invalid partition!  Must be "train" or "validation"')
+        
+    prediction = model.predict(np.expand_dims(data_point, axis=0))
+    return (audio_path,data_point,transcr,prediction)
